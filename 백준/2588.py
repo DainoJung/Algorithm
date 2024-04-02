dbf5c97b5537311e1ae1cf78597bb053478dce7f
DainJung <wjdekdls3693@gmail.com>
2024-04-02 13:41:05 +0900

    Create 2588.py

---
+++ 백준/2588.py
@@ -0,0 +1,7 @@
+firstNum = int(input())
+secondNum = input()
+
+print(firstNum * int(secondNum[2]))
+print(firstNum * int(secondNum[1]))
+print(firstNum * int(secondNum[0]))
+print(firstNum * int(secondNum))
